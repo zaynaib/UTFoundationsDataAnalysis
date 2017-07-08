c640af8895bbcc3b0e158730f0e0e022d33a8470
Zaynaib Giwa <zaynaibg@gmail.com>
2017-07-07 23:25:00 -0500

    Lab 2 of UT Foundations of Data Analysis
    Corrected the R file and separated pre lab code from lab code.
    Still have to finish lab 2 tonight.

--- Lab2.R
+++ Lab2.R
@@ -1,3 +1,7 @@
+#############################
+# Pre Lab                   #
+#############################
+
 #read the data from the csv file
 animaldata<-read.csv("AnimalData.csv")
 
@@ -12,8 +16,53 @@ head(animaldata, n=10)
 #OR we can do some subset magic
 outcome <- animaldata$Outcome.Type
 head(outcome,n=10)
+adopted <- animaldata[animaldata$Outcome.Type =="Adoption",]
 
+daystoadopt <-adopted$Days.Shelter
 #Was the first owner-surrendered animal in the dataset neutered?
 #Neutered.Status
 #https://www.r-bloggers.com/which-function-in-r/
-which()
+which(daystoadopt>150)
+plot(animaldata$daystoadopt)
+
+
+#############################
+#     Lab                   #
+#############################
+
+#Access CookBook
+library(SDSFoundations)
+?AnimalData
+
+#Show how many adult(at intake) cats and dogs are in the dataset
+table(animaldata$Animal.Type)
+
+#how many adult dogs are in the shelter
+dogs <- animaldata[animaldata$Animal.Type =="Dog",]
+adultDogs<-dogs[dogs$Age.Intake>0,]
+nrow(adultDogs)
+
+#how many adult cats are in the shelter
+cats <- animaldata[animaldata$Animal.Type =="Cat",]
+adultCats<-cats[cats$Age.Intake>0,]
+nrow(adultCats)
+
+#what is the shape of distrbution of weight for adult dogs?
+hist(adultDogs$Weight)
+
+#what is the shape of distrbution of weight for adult cats?
+hist(adultCats$Weight)
+
+#Which measure of center should be used to describe the average weight of the adult cats?
+#mean
+
+#Average adult cat weight in pound
+mean(adultCats$Weight)
+
+#What the standard deviation of adult cats?
+sd(adultCats$Weight)
+
+
+
+
+
